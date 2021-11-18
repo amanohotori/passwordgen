562f5a74c06cc53d7413da1d4867f822f9e37a9f
Hotori Amano <amanohotori@gmail.com>
2021-11-18 09:38:37 +0900

    first commit

---
+++ passwordgen.py
@@ -0,0 +1,29 @@
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-l", "--pwdlength", help="Generating password length. (Integral value)",
+                    type=int, default=32, metavar=NotImplemented)
+parser.add_argument("-s", "--nosign", help="Without sign option.",
+                    action="store_false")
+args = parser.parse_args()
+
+import string
+import secrets
+
+chars = []
+
+lowercase = string.ascii_lowercase
+uppercase = string.ascii_uppercase
+digits = string.digits
+signs = "/-+.!#$%,_@"
+
+charset = lowercase + uppercase + digits
+
+if args.nosign:
+    charset = charset + signs
+
+while len(chars) <= args.pwdlength:
+    chars.append(secrets.choice(charset))
+else:
+    pwdstring = ''.join(chars)
+    print(pwdstring)
